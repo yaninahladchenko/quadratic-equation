ada0ccf56f33e7a1201eed432c3bc97af497956b
Yanina Hladchenko <yanina.hladchenko@gmail.com>
2019-03-06 17:49:06 +0200

    Fixed code style

--- src/main/java/QuadraticEquation.java
+++ src/main/java/QuadraticEquation.java
@@ -1,15 +1,13 @@
 public class QuadraticEquation {
-    public static void  main(String[] args) {
-        double a=10;
-        double b=20;
-        double c=30;
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 20;
+        double c = 30;
 
-        double x1;
-        x1=(-b+Math.sqrt(Math.abs(b*b-4*a*c)))/(2*a);
+        double x1 = (-b + Math.sqrt(Math.abs(b * b - 4 * a * c))) / (2 * a);
         System.out.println(x1);
 
-        double x2;
-        x2=(-b-Math.sqrt(Math.abs(b*b-4*a*c)))/(2*a);
+        double x2 = (-b - Math.sqrt(Math.abs(b * b - 4 * a * c))) / (2 * a);
         System.out.println(x2);
     }
 }
