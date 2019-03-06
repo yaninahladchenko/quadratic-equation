19a8eca64eb0d2527dd3a48a9134727544bb567b
Yanina Hladchenko <yanina.hladchenko@gmail.com>
2019-03-06 11:03:37 +0200

    Initial commit

---
+++ src/main/java/QuadraticEquation.java
@@ -0,0 +1,15 @@
+public class QuadraticEquation {
+    public static void  main(String[] args) {
+        double a=10;
+        double b=20;
+        double c=30;
+
+        double x1;
+        x1=(-b+Math.sqrt(Math.abs(b*b-4*a*c)))/(2*a);
+        System.out.println(x1);
+
+        double x2;
+        x2=(-b-Math.sqrt(Math.abs(b*b-4*a*c)))/(2*a);
+        System.out.println(x2);
+    }
+}
